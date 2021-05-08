bc00e31454268b598c1aa9353ceb9164e749483e
Salim66 <salimhasanriad8@gmail.com>
2021-05-09 01:36:01 +0600

    User Template add input file and successfully upload user with photo

--- inc/user.php
+++ inc/user.php
@@ -35,13 +35,20 @@ if ($action == 'create') {
     // $email = $data->email;
     // $cell = $data->cell;
 
-    //get all user data by $_POST[]
+    //get file name and file tmp name
+    $photo_name = $_FILES['photo']['name'];
+    $photo_tmp_name = $_FILES['photo']['tmp_name'];
+
+    //upload user photo
+    move_uploaded_file($photo_tmp_name, '../photos/users/' . $photo_name);
+
+    //get all user data by $_POST[] 
     $name = $_POST['name'];
     $email = $_POST['email'];
     $cell = $_POST['cell'];
 
-    //insert data query
-    $conn->query("INSERT INTO users (name, email, cell) VALUES ('$name', '$email', '$cell')");
+    // //insert data query
+    $conn->query("INSERT INTO users (name, email, cell, photo) VALUES ('$name', '$email', '$cell', '$photo_name')");
 }
 
 /**
